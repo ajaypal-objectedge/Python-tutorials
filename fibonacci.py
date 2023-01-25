e89840116f21663b143476b882bda0211549252b
Ajay <ajaypal2961@gmail.com>
2023-01-25 16:11:09 +0530

    modified fibonacci and ignored some files

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+# print fibonacci series for N terms 
+
+n = input("enter the value for n")
+printFibonacciNumbers(n)
+
+def  printFibonacciNumbers(n):
+    a = int(0)
+    b = int(1)
+    c = int(1)
+
+    print(0)
+    print(1)
+    
+   
+
